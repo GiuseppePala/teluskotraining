8c7952401bf99f549069095f9446bffe623b4de4
GiuseppePala <32236478+giuseppepala@users.noreply.github.com>
2019-10-21 18:53:34 +0200

    Ho aggiunto qualche commento, e verificato l'efficienza del programma con dati alfanumerici.

--- bubblesort.py
+++ bubblesort.py
@@ -1,10 +1,12 @@
 # Bubble sort
+# Ordinamento degli elementi di una lista, perfetto per valori sia nimerici che alfanumerici
 def sort(lista):
     for cicli in range(len(lista)-1, 0, -1):
         for riga in range(cicli):
             if lista[riga] > lista[riga + 1]:
                 lista[riga], lista[riga + 1] = lista[riga + 1], lista[riga]
 
-miaLista=[23, 5, 78, 1211, 3, 56, 78, 12, 4, 51, 4096, 255]
+#miaLista=[23, 5, 78, 1211, 3, 56, 78, 12, 4, 51, 4096, 255]
+miaLista=["Simone", "Piera", "Carlo", "Susanna", "Marina", "Giulia", "Clio", "Savera", "Gemma", "Laura", "Claudio", "Sergio", "Marco"]
 sort(miaLista)
 print(miaLista)
